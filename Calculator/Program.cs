2e5d0392bc2336bcc4a0eee651828d9c181a8e77
K-Joshua <saintangel9989@gmail.com>
2025-02-11 21:10:01 +0800

    kanade

---
+++ Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using CalculatorLibrary;
+
+int a = 0, b = 0;
+
+Console.Write("Enter value for the first number: ");
+a = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Enter value for the second number: ");
+b = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"The sum of {a} and {b} is {CalculatorLibrary.Formulas.Basic.Addition(a, b)}");
+Console.WriteLine($"The difference of {a} and {b} is {CalculatorLibrary.Formulas.Basic.Subtraction(a, b)}");
---
+++ KanadeYoisaki/Program.cs
@@ -0,0 +1,11 @@
+﻿using CalculatorLibrary.Formulas;
+
+Console.Write("Enter first number: ");
+decimal.TryParse(Console.ReadLine(), out var firstNumber);
+Console.Write("Enter second number: ");
+decimal.TryParse(Console.ReadLine(), out var secondNumber);
+
+BasicWithProperties.FirstNumber = firstNumber;
+BasicWithProperties.SecondNumber = secondNumber;
+
+Console.WriteLine(BasicWithProperties.getAddition());
