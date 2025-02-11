db9a503abd35d23a3362a4599e750cafe44d367f
kyla <kylamarie.caballero@dbtc-cebu.edu.ph>
2025-02-11 15:32:11 +0800

    new file

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using CalculatorLibrary;
+using CalculatorLibrary.Formulas;
+
+Console.Write("Enter first number: ");
+decimal.TryParse(Console - ReadLine(), out var firstNumber);
+Console.Write"Enter second number: “);
+decimal.TryParse(Console: ReadLine(), out var secondNumber);
+
+BasicWithProperties.FirstNumber = firstNumber;
+BasicWithProperties.SecondNumber = secondNumber;
+Console.WriteLine(BasicWithProperties.GetAddition());
---
+++ LibraryWithProperties/Program.cs
@@ -0,0 +1,11 @@
+﻿using CalculatorLibrary;
+using CalculatorLibrary.Formulas;
+
+Console.Write("Enter first number: ");
+decimal.TryParse(Console.ReadLine(), out var FirstNumber);
+Console.Write("Enter second number: ");
+decimal.TryParse(Console.ReadLine(), out var SecondNumber);
+
+BasicwithProperties.FirstNumber = FirstNumber;
+BasicwithProperties.SecondNumber = SecondNumber;
+Console.WriteLine(BasicwithProperties.GetAddition());
