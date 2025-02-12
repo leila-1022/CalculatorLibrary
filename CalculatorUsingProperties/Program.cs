9ad039eb2049bc569a4ee6fa14239ee17d1969ce
leilabangoy <leilabangoy@gmail.com>
2025-02-12 11:48:11 +0800

    PropertiesLibrary

---
+++ CalculatorUsingProperties/Program.cs
@@ -0,0 +1,12 @@
+﻿using CalculatorLibrary;
+using CalculatorLibrary.Formulas;
+
+Console.Write("Enter first number: ");
+decimal.TryParse(Console.ReadLine(), out var firstNumber);    
+Console.Write("Enter second number: ");
+decimal.TryParse(Console.ReadLine(), out var secondNumber);
+
+BasicWithProperties.FirstNumber  = firstNumber;
+BasicWithProperties.SecondNumber = secondNumber;
+
+Console.WriteLine(BasicWithProperties.GetAddition());
