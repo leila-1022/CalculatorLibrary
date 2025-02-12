7a5ea87fb0e6d9fa464970f121b6ea44c96993d5
SHAIRHA <shairhamay.ejares@dbtc-cebu.edu.ph>
2025-02-12 11:08:32 +0800

    Activity1

---
+++ CalculatorE/Program.cs
@@ -0,0 +1,13 @@
+﻿using CalculatorLibraryE;
+using CalculatorLibraryE.Formulas;
+
+int a = 0, b = 0;
+
+Console.Write("Enter value for the first number: ");
+a = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Enter value for the second number: ");
+b = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"The sum of {a} and {b} is {Basic.Addition(a, b)}");
+Console.WriteLine($"The difference of {a} and {b} is {Basic.Subtraction(a, b)}");
---
+++ ToukaKirishima/Program.cs
@@ -0,0 +1,12 @@
+﻿using CalculatorLibraryE;
+using CalculatorLibraryE.Formulas;
+
+Console.Write("Enter first number: ");
+decimal.TryParse(Console.ReadLine(), out var firstNumber);
+Console.Write("Enter second number: ");
+decimal.TryParse(Console.ReadLine(), out var secondNumber);
+
+BasicWithProperties.FirstNumber = firstNumber; 
+BasicWithProperties.SecondNumber = secondNumber;
+
+Console.WriteLine(BasicWithProperties.getAddition());
