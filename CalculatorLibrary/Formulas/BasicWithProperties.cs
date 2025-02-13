fb932853432d74f4e47788ad5dc7224075fab310
bryankinyo <bryan.quino@dbtc-cebu.edu.ph>
2025-02-13 13:05:36 +0800

    LibraryWithProperties

---
+++ CalculatorLibrary/Formulas/BasicWithProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.Formulas
+{
+    public static class BasicWithProperties
+    {
+        public static decimal FirstNumber { get; set; }
+        public static decimal SecondNumber { get; set; }
+
+        public static string GetAddition()
+        {
+            return $"The sum of {FirstNumber} and {SecondNumber} is {FirstNumber + SecondNumber}";
+        }
+        public static string GetSubtraction()
+        {
+            return $"The difference of {FirstNumber} and {SecondNumber} is {FirstNumber - SecondNumber}";
+        }
+        public static string GetMultiplication()
+        {
+            return $"The product of {FirstNumber} and {SecondNumber} is {FirstNumber * SecondNumber}";
+        }
+        public static string GetDivision()
+        {
+            if (SecondNumber == 0)
+                throw new Exception("Denominator cannot be zero.");
+
+            return $"The quotient of {FirstNumber} and {SecondNumber} is {FirstNumber / SecondNumber}";
+        }
+    }
+}
---
+++ PropertiesApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using CalculatorLibrary;
+using CalculatorLibrary.Formulas;
+
+Console.Write("Enter first number: ");
+decimal.TryParse(Console.ReadLine(), out var firstNumber);
+Console.Write("Enter second number: ");
+decimal.TryParse(Console.ReadLine(), out var secondNumber);
+
+BasicWithProperties.FirstNumber = firstNumber;
+BasicWithProperties.SecondNumber = secondNumber;
+
+Console.WriteLine(BasicWithProperties.GetAddition());
+Console.WriteLine(BasicWithProperties.GetSubtraction());
+Console.WriteLine(BasicWithProperties.GetMultiplication());
+Console.WriteLine(BasicWithProperties.GetDivision());
