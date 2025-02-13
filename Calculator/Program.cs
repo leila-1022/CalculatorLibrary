1cc56376914c622a51fcac7ccc6f486c86efcea5
leilabangoy <leilabangoy@gmail.com>
2025-02-13 13:11:15 +0800

    Issue #1

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,4 @@
-﻿using CalculatorLibrary;
-
+﻿
 ///Flow
 ///
 int a = 0, b = 0;
@@ -9,9 +8,3 @@ a = int.Parse(Console.ReadLine() ?? "0");
 
 Console.Write("Enter value for the second number: ");
 b = int.Parse(Console.ReadLine() ?? "0");
-
-Console.WriteLine($"The sum of {a} and {b} is {CalculatorLibrary.Formulas.Basic.Addition(a, b)}");
-Console.WriteLine($"The difference of {a} and {b} is {CalculatorLibrary.Formulas.Basic.Subtraction(a, b)}");
-Console.WriteLine($"The product of {a} and {b} is {CalculatorLibrary.Formulas.Basic.Multiplication(a,b)}");
-Console.WriteLine($"The quocent of {a} and {b} is {CalculatorLibrary.Formulas.Basic.Division(a,b)}");
-
