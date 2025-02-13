95a9d7e7f0fe8bc5880bb5ce5de56ba0cc88418b
invalid-code <jessgaming55@gmail.com>
2025-02-13 20:28:41 +0800

    first year examination

--- CalculatorLibrary/Formulas/Shapes/Rectangle.cs
+++ CalculatorLibrary/Formulas/Shapes/Rectangle.cs
@@ -9,7 +9,17 @@ namespace CalculatorLibrary.Formulas.Shapes
     /// <summary>
     /// Add methods and properties to get the rectangle's Area and Perimeter
     /// </summary>
-    internal class Rectangle
+    public class Rectangle
     {
+        public static double width {get; set;}
+        public static double height {get; set;}
+
+        public static string getArea() {
+            return $"the area is {width * height}";
+        }
+
+        public static string getPerimeter() {
+            return $"the perimeter is {2 * (width + height)}";
+        }
     }
 }
---
+++ ShapesCalculator/Program.cs
@@ -0,0 +1,114 @@
+﻿/*
+Group WaKoKabaw
+Members:
+    Sucgang
+    Cabaluna
+    Evangelista
+*/
+using CalculatorLibrary.Formulas.Shapes;
+using System;
+
+Console.Write("*********************");
+Console.Write("*                   *");
+Console.Write("* Shapes Calculator *");
+Console.Write("* 1. Circle         *");
+Console.Write("* 2. Square         *");
+Console.Write("* 3. Rectangle      *");
+Console.Write("*                   *");
+Console.Write("*********************");
+Console.Write("shape to select(1-3): ");
+try
+{
+    int choice = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    switch (choice)
+    {
+        case 1:
+            Console.Write("radius: ");
+            Circle.radius = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("************************");
+            Console.Write("*                      *");
+            Console.Write("* Circle Calculator    *");
+            Console.Write("* 1. get Area          *");
+            Console.Write("* 2. get Circumference *");
+            Console.Write("*                      *");
+            Console.Write("************************");
+            Console.Write("get circle properties(1-2): ");
+            int choiceFunc = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (choiceFunc)
+            {
+                case 1:
+                    Console.WriteLine($"area of the circle is {Circle.getArea()}");
+                    break;
+                case 2:
+                    Console.WriteLine($"circumference of the circle is {Circle.getCircumference()}");
+                    break;
+                default:
+                    Console.WriteLine("Unknown property given");
+                    break;
+            }
+            break;
+        case 2:
+            Console.Write("side length: ");
+            Square.sideLength = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("*********************");
+            Console.Write("*                   *");
+            Console.Write("* Square Calculator *");
+            Console.Write("* 1. get Area       *");
+            Console.Write("* 2. get Perimeter  *");
+            Console.Write("*                   *");
+            Console.Write("*********************");
+            Console.Write("get square properties(1-2): ");
+            int choiceFunc = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (choiceFunc)
+            {
+                case 1:
+                    Console.WriteLine($"area of the circle is {Square.getArea()}");
+                    break;
+                case 2:
+                    Console.WriteLine($"perimeter of the circle is {Square.getPerimeter()}");
+                    break;
+                default:
+                    Console.WriteLine("Unknown property given");
+                    break;
+            }
+            break;
+        case 3:
+            Console.Write("height: ");
+            Rectangle.height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("width: ");
+            Rectangle.width = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.Write("************************");
+            Console.Write("*                      *");
+            Console.Write("* Rectangle Calculator *");
+            Console.Write("* 1. get Area          *");
+            Console.Write("* 2. get Perimeter     *");
+            Console.Write("*                      *");
+            Console.Write("************************");
+            Console.Write("get rectangle properties(1-2): ");
+            int choiceFunc = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (choiceFunc)
+            {
+                case 1:
+                    Console.WriteLine($"area of the circle is {Rectangle.getArea()}");
+                    break;
+                case 2:
+                    Console.WriteLine($"perimeter of the circle is {Rectangle.getPerimeter()}");
+                    break;
+                default:
+                    Console.WriteLine("Unknown property given");
+                    break;
+            }
+            break;
+        default:
+            Console.WriteLine("Unknown shape given");
+            break;
+    }
+}
+catch { }
