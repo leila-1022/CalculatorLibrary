d3ce8203eba641f795a23421ce3291fd7a597161
K-Joshua <saintangel9989@gmail.com>
2025-02-14 10:52:26 +0800

    TriBugWeebos

--- CalculatorLibrary/Formulas/Shapes/Rectangle.cs
+++ CalculatorLibrary/Formulas/Shapes/Rectangle.cs
@@ -9,7 +9,18 @@ namespace CalculatorLibrary.Formulas.Shapes
     /// <summary>
     /// Add methods and properties to get the rectangle's Area and Perimeter
     /// </summary>
-    internal class Rectangle
+    public static class Rectangle
     {
+        public static double Area(double length, double width)
+        {
+            double Area_2 = length * width; ;
+            return Math.Round(Area_2, 2);
+        }
+
+        public static double Perimeter(double length, double width)
+        {
+            double Perimeter_2 = 2 * (length + width);
+            return Math.Round(Perimeter_2, 2);
+        }
     }
 }
