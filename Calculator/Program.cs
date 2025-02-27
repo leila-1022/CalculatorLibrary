025fbbd86f60938984234ed8c68464d60b47f1d7
Ezik143 <jakesucgang90@gmail.com>
2025-02-28 01:59:05 +0800

    Added Linear Equations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿
 ///Flow
 ///
@@ -8,3 +9,36 @@ a = int.Parse(Console.ReadLine() ?? "0");
 
 Console.Write("Enter value for the second number: ");
 b = int.Parse(Console.ReadLine() ?? "0");
+=======
+﻿using CalculatorLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int firstNumber = 0, secondNumber = 0;
+
+        Console.Write("Enter value for the first number: ");
+        if (int.TryParse(Console.ReadLine() ?? "0", out firstNumber))
+        {
+            Console.Write("Enter value for the second number: ");
+            if (int.TryParse(Console.ReadLine() ?? "0", out secondNumber))
+            {
+                Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {CalculatorLibrary.Formulas.Basic.Addition(firstNumber, secondNumber)}");
+                Console.WriteLine($"The difference of {firstNumber} and {secondNumber} is {CalculatorLibrary.Formulas.Basic.Subtraction(firstNumber, secondNumber)}");
+                Console.WriteLine($"The product of {firstNumber} and {secondNumber} is {CalculatorLibrary.Formulas.Basic.Multiplication(firstNumber, secondNumber)}");
+                Console.WriteLine($"The quotient of {firstNumber} and {secondNumber} is {CalculatorLibrary.Formulas.Basic.Division(firstNumber, secondNumber)}");
+            }
+            else
+            {
+                Console.WriteLine("Error! Please input a valid number for the second number.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error! Please input a valid number for the first number.");
+        }
+    }
+}
+>>>>>>> Saitama_Examination_2
---
+++ Linear-Equation-Main-Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CalculatorLibrary.Formulas.Linear_Equation;
+
+class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter slope: ");
+                double slope = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter intercept: ");
+                double intercept = Convert.ToDouble(Console.ReadLine());
+
+                var equation = new LinearEquation(slope, intercept);
+
+                Console.Write("Enter x value: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                double y = equation.CalculateY(x);
+                Console.WriteLine($"The value of y for x = {x} is: {y}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter numeric values.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            Console.Write("Do you want to try again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+            if (response != "yes")
+            {
+                break;
+            }
+        }
+    }
+}
