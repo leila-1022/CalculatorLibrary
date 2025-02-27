8ccd6a0243e7abdf4030afbc3c827390bb2bdff3
clentone <clentone.guanzon@dbtc-cebu.edu.ph>
2025-02-27 19:34:04 +0800

    LinearEquation_Buntis_V

---
+++ LinearEquation/LinearEquation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LinearEquationFormula;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter value for slope: ");
+        double slope = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter value for intercept: ");
+        double intercept = Convert.ToDouble(Console.ReadLine());
+
+
+        LinearEquation equation = new LinearEquation(slope, intercept);
+
+        Console.WriteLine("Enter value for x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double y = equation.CalculateY(x);
+        Console.WriteLine($"The value of y for x = {x} is: {y}");
+
+    }
+}
---
+++ LinearEquation/LinearFormula/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace LinearEquationFormula
+{
+    public class LinearEquation
+    {
+        public double Slope { get; set; }
+        public double Intercept { get; set; }
+
+        public LinearEquation(double slope, double intercept)
+        {
+            Slope = slope;
+            Intercept = intercept;
+        }
+
+
+        public double CalculateY(double x)
+        {
+            return Slope * x + Intercept;
+        }
+    }
+}
