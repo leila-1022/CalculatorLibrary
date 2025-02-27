b4f0f9163eda0a9c6179a97bddb01c1c16fb854d
dominic-netizen <dominic.ocarol@dbtc-cebu.edu.ph>
2025-02-27 15:34:42 +0800

    new commit

---
+++ CalculatorLibrary/Formulas/LQFormulas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.Formulas
+{
+    public class LQFormulas
+    {
+        public double slope;
+        public double intercept;
+        public double x;
+        public double linear
+        {
+            get
+            {
+                double y = (slope * x) + intercept;
+                return y;
+            }
+        }
+    }
+}
