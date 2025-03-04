4ce1f1878d28be79464caeeaf7da565a6afafa20
Seiseiyah <vincent.rebese@dbtc-cebu.edu.ph>
2025-03-04 14:45:44 +0800

    Initial commit

---
+++ CalculatorLibraryV2/ConsumingModelCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalculatorLibraryV2.Models;
+using System.Globalization;
+
+Numbers _number = new Numbers();
+Console.Write("How many numbers do you want to enter?: ");
+var counter = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < counter; i++)
+{
+    Console.WriteLine($"Enter number { i + 1}: ");
+    var inputtedNumber = Convert.ToInt32(Console.ReadLine());
+    _number.ListOfNumbers.Add(inputtedNumber);
+}
+Console.WriteLine("You entered: ");
+foreach (var item in _number.ListOfNumbers)
+{
+    Console.WriteLine(item. ToString() + " " );
+}
