095f508d27187b7e08469c582e1913b10a0d6407
K-Joshua <saintangel9989@gmail.com>
2025-03-04 14:49:48 +0800

    UnitTestSample Santos

---
+++ BasicUnitTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using CalculatorLibrary1.Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var addition = Basic.Addition(1, 1);
+            Assert.AreEqual(addition, 2);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var subtraction = Basic.Subtraction(1, 1);
+            Assert.AreEqual(subtraction, 0);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var multiplication = Basic.Multiplication(1, 1);
+            Assert.AreEqual(multiplication, 1);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var division = Basic.Division(1, 1);
+            Assert.AreEqual(division, 1);
+        }
+    }
+}
---
+++ ConsumingModelCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using CalculatorLibrary1.Models;
+
+double Average;
+
+Numbers _number = new Numbers();
+_number.ListOfNumbers = new List<int>();
+Console.WriteLine("How many numbers do you want to enter?: ");
+
+var counter = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < counter; i++)
+{
+    Console.Write($"Enter number {i + 1}: ");
+    var inputtedNumber = Convert.ToInt32(Console.ReadLine());
+    _number.ListOfNumbers.Add(inputtedNumber);
+}
+Average = _number.ListOfNumbers.Sum() / (double)_number.ListOfNumbers.Count;
+
+Console.WriteLine("You entered: ");
+foreach (var item in _number.ListOfNumbers)
+{
+    Console.WriteLine(item.ToString() + " ");
+}
+{
+    Console.WriteLine("The Total Average is " + Average);
+}
+
+int sum1, sum2, total;
+
+Console.WriteLine("Input a number: ");
+sum1 = _number.ListOfNumbers.Sum();
+Console.WriteLine("Input a number: ");
+sum2 = _number.ListOfNumbers.Sum();
+
+total = _number.ListOfNumbers.Count();
+Console.WriteLine();
+
+if (sum1 > sum2)
+{
+    Console.WriteLine();
+}
+else if (sum2 > total)
+{
+    Console.WriteLine();
+}
+
+
