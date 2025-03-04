c206c036d3e27c66a16411bca049e5cbb34d3391
Cassandra-Oraiz <cassandragayle.oraiz@dbtc-cebu.edu.ph>
2025-03-04 13:29:47 +0800

    BackUp_for_Examination

---
+++ CalculatorLibrary/Formulas/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.Formulas.Models
+{
+    public class Numbers
+    {
+        public List<int> ListOfNumbers { get; set; }
+
+        public double Average { get; set; }
+    }
+}
---
+++ ConsumingModelCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorLibrary.Formulas.Models;
+
+Numbers _number = new Numbers();
+_number.ListOfNumbers = new List<int>();
+Console.WriteLine("How many numbers do you want to enter?: ");
+var counter = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < counter; i++)
+{
+    Console.Write($"Enter number {i + 1}: ");
+    var inputtedNumber = Convert.ToInt32(Console.ReadLine());
+    _number.ListOfNumbers.Add(inputtedNumber);
+    _number.Average += inputtedNumber;
+}
+
+Console.WriteLine("You entered: ");
+foreach (var item in _number.ListOfNumbers)
+{
+    Console.Write(item.ToString() + " ");
+}
+
+Console.WriteLine($"\nThe average of the numbers is: {_number.Average / counter}");
