79ecb22a6a6153ce829669a785bfac8752c52730
Ezik143 <jakesucgang90@gmail.com>
2025-03-04 14:17:25 +0800

    hehehe

--- BasicUnitTest/UnitTest1.cs
+++ BasicUnitTest/UnitTest1.cs
@@ -7,11 +7,17 @@ namespace BasicUnitTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [TestMethod]    
         public void TestMethod1()
         {
            var addition = Basic.Addition(1, 1);
             Assert.AreEqual(addition, 2);
+            var subtraction = Basic.Subtraction(1,1);
+            Assert.AreEqual(subtraction, 0);
+            var multiplication = Basic.Multiplication(1,1);
+            Assert.AreEqual(multiplication, 1);
+            var division = Basic.Division(1,1);
+            Assert.AreEqual(division, 1);
         }
     }
 }
