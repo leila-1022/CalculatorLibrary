1cc176214ec19d7320c5dc9055b5a52b2ebc87af
Seiseiyah <vincent.rebese@dbtc-cebu.edu.ph>
2025-03-04 16:05:25 +0800

    UnitTest

---
+++ CalculatorLibraryV2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using CalculatorLibraryV2.Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BasicUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var addition = Basic.Addition(addition, 1);
+            Assert.Equals(addition, 2);
+        }
+        public void TestMethod2()
+        {
+            var subtraction = Basic.Subtraction(subtraction, 1);
+            Assert.Equals(subtraction, 0);
+        }
+        public void TestMethod3()
+        {
+            var multiplication = Basic.Multiplication(multiplication, 1);
+            Assert.Equals(multiplication, 1);
+        }
+        public void TestMethod4()
+        {
+            var division = Basic.Division(division, 1);
+            Assert.Equals(division, 1);
+        }
+    }
+}
