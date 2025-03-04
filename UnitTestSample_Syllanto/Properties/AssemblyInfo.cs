a3fa1752d1e77b4ec605770167c51a16d52cdd1b
MjSyllanto <maryjocelyn.syllanto@dbtc-cebu.edu.ph>
2025-03-04 14:36:02 +0800

    initial commit

---
+++ UnitTestSample_Syllanto/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSample_Syllanto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSample_Syllanto")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c1c01eb-f16a-4bf7-bc47-e56da9d70a56")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestSample_Syllanto/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using CalculatorLibrary.Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BasicUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var addition = Basic.Addition(1, 1);
+            Assert.AreEqual(addition, 2);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var subtraction = Basic.Subtraction(3, 2);
+            Assert.AreEqual(subtraction, 1);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var multiplication = Basic.Multiplication(3, 2);
+            Assert.AreEqual(multiplication, 6);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var division = Basic.Division(4, 2);
+            Assert.AreEqual(division, 2);
+        }
+    }
+}
