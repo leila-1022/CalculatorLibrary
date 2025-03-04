318ce67356ec6a833634d7f0fe3d9306e687b896
Cassandra-Oraiz <cassandragayle.oraiz@dbtc-cebu.edu.ph>
2025-03-04 13:50:54 +0800

    initial commit

---
+++ BasicUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b290c92d-4b0d-45bf-af3f-ef699a3ce6a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BasicUnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using CalculatorLibrary.Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BasicUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           var addition = Basic.Addition(1, 1);
+            Assert.AreEqual(addition, 2);
+        }
+    }
+}
