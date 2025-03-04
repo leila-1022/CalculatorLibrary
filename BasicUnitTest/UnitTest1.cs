926ff19db124a15e57d8399719a64c143d02eb6e
Cassandra-Oraiz <cassandragayle.oraiz@dbtc-cebu.edu.ph>
2025-03-04 14:31:43 +0800

    Commit Changes

--- BasicUnitTest/UnitTest1.cs
+++ BasicUnitTest/UnitTest1.cs
@@ -13,5 +13,23 @@ namespace BasicUnitTest
            var addition = Basic.Addition(1, 1);
             Assert.AreEqual(addition, 2);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var subtraction = Basic.Subtraction(1, 1);
+            Assert.AreEqual(subtraction, 0);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var multiplication = Basic.Multiplication(1, 1);
+            Assert.AreEqual(multiplication, 1);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var division = Basic.Division(1, 1);
+            Assert.AreEqual(division, 1);
+        }
     }
 }
