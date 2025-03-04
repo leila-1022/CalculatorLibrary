7deb2642d0b8ee7b25361ad4aa1e1c99609e2d73
KVerano <kierstien.verano@dbtc-cebu.edu.ph>
2025-03-04 14:37:15 +0800

    UnitTestSample_Verano

--- BasicUnitTest/UnitTest1.cs
+++ BasicUnitTest/UnitTest1.cs
@@ -10,8 +10,26 @@ namespace BasicUnitTest
         [TestMethod]
         public void TestMethod1()
         {
-           var addition = Basic.Addition(1, 1);
+            var addition = Basic.Addition(1, 1);
             Assert.AreEqual(addition, 2);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var subtraction = Basic.Subtraction(3, 2);
+            Assert.AreEqual(subtraction, 1);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var multiplication = Basic.Multiplication(3, 2);
+            Assert.AreEqual(multiplication, 6);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var division = Basic.Division(4, 2);
+            Assert.AreEqual(division, 2);
         }
     }
 }
---
+++ UnitTestSample_Verano/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSample_Verano")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSample_Verano")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36247e8a-6271-4e1e-a2f3-e004f15fc951")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
